055321c5566a38627a53db12bfde9a0adbe8f1ec
chan <wognsl34@gmail.com>
2021-02-09 16:55:54 +0900

    add readme.md

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
-// dev JS
+/*
+ * @author chan
+ * @date 2021.02.09
+ * @version 1.0
+ */
 "use strict";
 const _FILTER = "win16|win32|win64|mac|macintel";
-
 const _USERAGENT = navigator.userAgent.toLowerCase();
 const _DEVICE = divisionDevice();
 const _DEVICE_SYSTEM = _USERAGENT.indexOf("iphone") > -1 ? "iOS" : "Android";
@@ -15,18 +18,3 @@ function divisionDevice() {
         }
     }
 }
-
-function exitBrowser() {
-    let str = "";
-    str += "userAgent 출력 " + _USERAGENT + "\n";
-    str += "Device 출력 " + _DEVICE + "\n";
-    str += "Device 운영체제 출력 " + _DEVICE_SYSTEM;
-    alert(str);
-    // if (userAgent.indexOf("kakao") != -1 || userAgent.indexOf("naver") != -1) {
-    //     return true;
-    // } else {
-    //     return false;
-    // }
-}
-
-exitBrowser();
