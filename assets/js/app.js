738bdac42e8d58b19988c982150a18ec855a3f3c
chan <wognsl34@gmail.com>
2021-02-09 16:39:03 +0900

    update userAgent Print

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,32 @@
 // dev JS
 "use strict";
-const userAgent = navigator.userAgent.toLowerCase();
-let isKakao = checkKakao() ? "Y" : "N";
+const _FILTER = "win16|win32|win64|mac|macintel";
 
-function checkKakao() {
-    if (userAgent.indexOf("kakao") != -1) return true;
-    else return false;
+const _USERAGENT = navigator.userAgent.toLowerCase();
+const _DEVICE = divisionDevice();
+const _DEVICE_SYSTEM = _USERAGENT.indexOf("iphone") > -1 ? "iOS" : "Android";
+
+function divisionDevice() {
+    if (navigator.platform) {
+        if (_FILTER.indexOf(navigator.platform.toLowerCase()) < 0) {
+            return "Mobile";
+        } else {
+            return "PC";
+        }
+    }
 }
 
-alert(isKakao);
+function exitBrowser() {
+    let str = "";
+    str += "userAgent 출력 " + _USERAGENT + "\n";
+    str += "Device 출력 " + _DEVICE + "\n";
+    str += "Device 운영체제 출력 " + _DEVICE_SYSTEM;
+    alert(str);
+    // if (userAgent.indexOf("kakao") != -1 || userAgent.indexOf("naver") != -1) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+}
+
+exitBrowser();
