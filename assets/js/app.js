5f21b627d203b1c82961abc4caa554b1c005c1bb
chan <wognsl34@gmail.com>
2021-02-09 16:09:55 +0900

    update help minju

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,71 +1,84 @@
-var scroll = function () {
-    var $cnt = null,
-        moveCnt = null,
-        moveIndex = 0,
-        moveCntTop = 0,
-        winH = 0,
-        time = false; // 새로 만든 변수
+$(function () {
+    motionFn();
+});
 
-    $(document).ready(function () {
-        init();
-        initEvent();
-    });
+function motionFn() {
+    var wheelTime = 0,
+        flag = 0;
+    var $All = $(".wrapper"),
+        $scroll = $(".box");
 
-    var init = function () {
-        $cnt = $(".content");
-    };
+    // 휠
+    function wheelEvt() {
+        $("body").on("mousewheel DOMMouseScroll", function (e) {
+            if (flag) return;
 
-    var initEvent = function () {
-        $("html ,body").scrollTop(0);
-        winResize();
-        $(window).resize(function () {
-            winResize();
+            var evt = e.originalEvent,
+                wheel = evt.deltaY;
+            evt.detail ? (wheel = evt.detail) : (wheel = evt.deltaY); // detail == 파이어폭스
+            // 1 == 아래 / 0 == 위
+            wheel > 0 ? pageMove(1) : pageMove(0);
         });
-        $cnt.on("mousewheel", function (e) {
-            if (time === false) {
-                // time 변수가 펄스일때만 휠 이벤트 실행
-                wheel(e);
-            }
-        });
-    };
+    }
+    wheelEvt();
 
-    var winResize = function () {
-        winH = $(window).height();
-        $cnt.children("div").height(winH);
-        $("html ,body").scrollTop(moveIndex.scrollTop);
-    };
+    // 터치
+    function touchFn() {
+        var touchS, touchE;
 
-    var wheel = function (e) {
-        console.log(e);
-        if (e.originalEvent.wheelDelta < 0) {
-            if (moveIndex < 3) {
-                moveIndex += 1;
-                moving(moveIndex);
+        $scroll
+            .off("touchstart mousedown")
+            .on("touchstart mousedown", function (e) {
+                e.type == "touchstart"
+                    ? (touchS = e.originalEvent.targetTouches[0].clientY)
+                    : (touchS = e.clientY);
+            })
+            .off("touchend mouseup")
+            .on("touchend mouseup", function (e) {
+                e.type == "touchend"
+                    ? (touchE = e.originalEvent.changedTouches[0].clientY)
+                    : (touchE = e.clientY);
+
+                if (flag) return;
+                if (touchS - touchE > 100) {
+                    pageMove(1);
+                } else if (touchS - touchE < -100) {
+                    pageMove(0);
+                } else {
+                    return;
+                }
+            });
+    }
+    touchFn();
+
+    function pageMove(m) {
+        if (m) {
+            wheelTime++;
+            if (wheelTime >= $scroll.length - 1) {
+                wheelTime = $scroll.length - 1;
             }
         } else {
-            if (moveIndex > 0) {
-                moveIndex -= 1;
-                moving(moveIndex);
+            wheelTime--;
+            if (wheelTime < 0) {
+                wheelTime = 0;
             }
         }
-    };
 
-    var moving = function (index) {
-        time = true; // 휠 이벤트가 실행 동시에 true로 변경
-        moveCnt = $cnt.children("div").eq(index);
-        moveCntTop = moveCnt.offset().top;
-        $("html ,body")
-            .stop()
-            .animate(
-                {
-                    scrollTop: moveCntTop,
-                },
-                1000,
-                function () {
-                    time = false; // 휠 이벤트가 끝나면 false로 변경
-                }
-            );
-    };
-};
+        var bHeight;
+        $(window)
+            .resize(function () {
+                bHeight = $(".box").outerHeight();
+            })
+            .resize();
 
-scroll();
+        flag = 1;
+        $(".wrapper").animate(
+            { top: -(bHeight * wheelTime) },
+            1000,
+            "easeInOutCubic",
+            function () {
+                flag = 0;
+            }
+        );
+    }
+}
